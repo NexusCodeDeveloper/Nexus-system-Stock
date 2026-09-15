29182d8e1e32a442ea1b3467556f6c14ee7292db
Tomas <tomasgomezzi36@gmail.com>
2026-09-15 12:25:00 -0300

    feat: carrito lateral con checkout

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -3,26 +3,44 @@ import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import MobileNav from './MobileNav';
 import NewNotificationAlert from '../alerts/NewNotificationAlert';
+import CartPanel from '../Cart/CartPanel';
 import { NotificationProvider } from '../../context/NotificationContext';
+import { CartProvider, useCart } from '../../context/CartContext';
 
-const Layout = () => {
+const LayoutInner = () => {
+  const { cart } = useCart();
+  const conCarrito = cart.length > 0;
+
   return (
-    <NotificationProvider>
-      <div className="flex h-screen bg-ios-bg overflow-x-hidden">
+    <div className="flex h-screen bg-ios-bg overflow-x-hidden">
+      <div className="hidden md:block shrink-0 relative w-[72px]">
         <Sidebar />
-        <div className="flex-1 flex flex-col min-w-0">
-          <Navbar />
-          <main className="flex-1 overflow-y-auto px-4 pt-4 pb-28 md:px-6 md:pb-6">
-            <div className="max-w-7xl mx-auto animate-ios-page">
-              <Outlet />
-            </div>
-          </main>
-        </div>
-        <MobileNav />
-        <NewNotificationAlert />
       </div>
-    </NotificationProvider>
+      <div className="flex-1 flex flex-col min-w-0">
+        <Navbar />
+        <main className="flex-1 overflow-y-auto px-4 pt-4 pb-28 md:px-6 md:pb-6">
+          <div className="max-w-7xl mx-auto animate-ios-page">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+      {conCarrito && (
+        <div className="hidden md:block shrink-0 w-[320px]">
+          <CartPanel />
+        </div>
+      )}
+      <MobileNav />
+      <NewNotificationAlert />
+    </div>
   );
 };
+
+const Layout = () => (
+  <NotificationProvider>
+    <CartProvider>
+      <LayoutInner />
+    </CartProvider>
+  </NotificationProvider>
+);
 
 export default Layout;
