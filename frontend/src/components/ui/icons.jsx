c0a617d81f0827255e9e429d261dfc353c70dc35
tomas <tomasgomezzi36@gmail.com>
2026-09-20 01:49:14 -0300

    feat(frontend): icono de empleados

--- frontend/src/components/ui/icons.jsx
+++ frontend/src/components/ui/icons.jsx
@@ -17,6 +17,7 @@ export const IconEyeOff = (p) => <Icon {...p} d="M3 3l18 18M10.6 10.7a3 3 0 003.
 export const IconBox = (p) => <Icon {...p} d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />;
 export const IconChart = (p) => <Icon {...p} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />;
 export const IconUsers = (p) => <Icon {...p} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />;
+export const IconUserPlus = (p) => <Icon {...p} d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />;
 export const IconReturn = (p) => <Icon {...p} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />;
 export const IconLogout = (p) => <Icon {...p} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />;
 export const IconCheck = (p) => <Icon {...p} d="M5 13l4 4L19 7" />;
